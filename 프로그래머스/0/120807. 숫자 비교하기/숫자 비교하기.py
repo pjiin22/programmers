e999998b27c888f3fd406964181055f48ea92ddd
parkjiin <118836624+pjiin22@users.noreply.github.com>
2024-10-29 17:15:54 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.py
@@ -0,0 +1,8 @@
+def solution(num1, num2):
+    answer = 0
+    if 0 <= num1 <= 10000 and 0 <= num2 <= 10000:
+        if num1 == num2:
+            answer = 1
+        else:
+            answer = -1
+    return answer
