1f705f4c9611b8507df9528297334ab377e577a6
parkjiin <118836624+pjiin22@users.noreply.github.com>
2025-02-10 17:46:36 +0900

    [level 0] Title: 약수 구하기, Time: 0.77 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
@@ -1,8 +1,6 @@
 def solution(n):
     answer = []
-    i = 1
-    while i <= n:
-        if n % i == 0:
+    for i in range(1,n+1):
+        if n%i==0:
             answer.append(i)
-        i+= 1
     return answer
