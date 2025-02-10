a38455a04c9060e57f640ef79293928e4ca91ff9
parkjiin <118836624+pjiin22@users.noreply.github.com>
2025-02-10 16:19:46 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -1,5 +1,7 @@
 def solution(my_string):
-    answer = my_string
-    for a in "aeiou":
-        answer = answer.replace(a, "")
-    return answer
+    k=''
+    for i in my_string:
+        if i != 'a' and i!='e'and i!='i' and i!='o' and i!='u':
+            k+=i
+            
+    return k
