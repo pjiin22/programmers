92483bef96c03b5a11cce3863696d949ef750910
parkjiin <118836624+pjiin22@users.noreply.github.com>
2025-02-10 14:04:33 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.py
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.py
@@ -1,7 +1,5 @@
 def solution(n):
-    i = 1
-    while i * i <= n:
-        if i * i == n:
-            return 1  # n이 제곱수인 경우
-        i += 1
-    return 2  # n이 제곱수가 아닌 경우
+    if n** (1/2) == int(n** (1/2)):
+        return 1
+    else:
+        return 2
