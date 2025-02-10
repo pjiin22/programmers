9c2305166c2e4045f9f07555dba746ffd03ff59a
parkjiin <118836624+pjiin22@users.noreply.github.com>
2025-02-10 13:11:42 +0900

    [level 0] Title: 나이 출력, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120820. 나이 출력/나이 출력.py
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.py
@@ -1,8 +1,2 @@
 def solution(age):
-    answer = 0
-    if age <= 23:
-        answer = 2022 - age + 1
-    else:
-        age1 = age - 22
-        answer = 2000 - age1 + 1
-    return answer
+    return 2022-age+1
