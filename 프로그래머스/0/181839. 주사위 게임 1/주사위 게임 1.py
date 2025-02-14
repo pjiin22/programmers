74e1b6f1b47e4638f7013d6dbac9196496fca00e
parkjiin <118836624+pjiin22@users.noreply.github.com>
2025-02-14 16:53:48 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.py
@@ -0,0 +1,10 @@
+def solution(a, b):
+    answer = 0
+    
+    if a%2!=0 and b%2!=0:
+        return a**2 + b**2
+    elif a%2!=0 or b%2!=0:
+        return 2*(a+b)
+    else:
+        return abs(a-b)
+    
